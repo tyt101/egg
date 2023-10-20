7917b497ed080d2ff40a064418bad246c0411b13
tangytin@163.com <tangytin@163.com>
2023-10-20 21:48:47 +0800

    init

---
+++ p_egg_back/app/router.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+  const { router, controller } = app;
+  router.get('/', controller.home.index);
+  // router.get('/user/:id', controller.home.user);
+  router.get('/user', controller.home.user);
+  router.post('/add', controller.home.add);
+  router.post('/add_user', controller.home.addUser);
+  router.post('/edit_user', controller.home.editUser);
+  router.post('/del_user', controller.home.delUser);
+};
---
+++ summerise_xmind/url.md
@@ -0,0 +1,11 @@
+## study url
+juejin => https://juejin.cn/book/6966551262766563328?enter_from=course_center&utm_source=course_center
+
+## mysql connect problem
+https://blog.csdn.net/weixin_44785498/article/details/112670838
+
+
+## mysql account
+root
+## mysql password
+123456
